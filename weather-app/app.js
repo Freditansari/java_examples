c1c6e3e82f1a786719f2b7a9034e7b72d776470a
ftansari <freditansari@gmail.com>
2018-10-27 16:36:59 +0700

    no comment

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,7 +3,11 @@ const request = require('request');
 request({
     url:'http://www.mapquestapi.com/geocoding/v1/address?key=KQ2jsNksQts8jYzX1GvJHudpVyn6ZGhv&location=1430%20rancho%20hills%20dr',
     json: true
-}, (errror, response, body) =>{
-    console.log(JSON.stringify(body, undefined, 4));
+}, (error, response, body) =>{
+    //console.log(JSON.stringify(body, undefined, 4));
+    console.log(`Address: ${body.results[0].locations[0].street}`);
+    console.log(`${body.results[0].locations[0].adminArea5Type}: ${body.results[0].locations[0].adminArea5}`);
+    console.log(`latitiude: ${body.results[0].locations[0].latLng.lat}`);
+    console.log(`latitiude: ${body.results[0].locations[0].latLng.lng}`);
 
 });
