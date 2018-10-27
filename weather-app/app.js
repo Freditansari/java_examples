40432518dfb5eb26bb638f91c01027c7a949e206
ftansari <freditansari@gmail.com>
2018-10-27 23:20:58 +0700

    address input for weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -12,11 +12,11 @@ const argv = yargs
 }).help().alias('help','h').argv;
 
 //encode and decode string for url
-var address = encodeURIComponent(argv);
-console.log(address.toString());
+ 
+var encodedAddress= encodeURIComponent(argv.address);
 
 request({
-    url:'http://www.mapquestapi.com/geocoding/v1/address?key=KQ2jsNksQts8jYzX1GvJHudpVyn6ZGhv&location=1430%20rancho%20hills%20dr',
+    url:`http://www.mapquestapi.com/geocoding/v1/address?key=KQ2jsNksQts8jYzX1GvJHudpVyn6ZGhv&location=${encodedAddress}`,
     json: true
 }, (error, response, body) =>{
     //console.log(JSON.stringify(body, undefined, 4));
