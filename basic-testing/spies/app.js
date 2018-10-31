922003ea42b5b9d2e7085773dd639436aafbf133
ftansari <freditansari@gmail.com>
2018-10-31 16:05:18 +0700

    connected to mongodb

--- basic-testing/spies/app.js
+++ basic-testing/spies/app.js
@@ -2,6 +2,6 @@ const db = require('./db');
 module.exports.handleSignUp =(email, password)=>{
     //check email already exist
     //save user to database
-    db.saveUser
+    db.saveUser({email, password});
     //send the welcome email
 };
