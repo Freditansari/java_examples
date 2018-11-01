46565b06efeef7ab298687e968beef12cb0db9e6
ftansari <freditansari@gmail.com>
2018-11-01 13:40:16 +0700

    added api end point

---
+++ node-todo-api/server/server.js
@@ -0,0 +1,37 @@
+/**
+ * https://mongoosejs.com/docs/validation.html
+ * 
+ * https://mongoosejs.com/docs/guide.html
+ */
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {Mongoose} = require('./db/mongoose');
+var {Todo} = require('./db/models/todo');
+var {User} = require('./db/models/user');
+
+var app=express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res)=>{
+    //this line is to check request made body  => console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc)=>{
+        res.send(doc);
+
+    }, (error)=>{
+        res.status(400).send(error);
+
+    });
+});
+
+app.listen(3000, ()=>{
+    console.log('server is started');
+});
+
+
+
