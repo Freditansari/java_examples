3f2349cfe1643ac77382605276b6a3e7d86ebde0
ftansari <freditansari@gmail.com>
2018-11-02 14:01:28 +0700

    added user sign up api

--- node-todo-api/server/db/models/user.js
+++ node-todo-api/server/db/models/user.js
@@ -1,10 +1,56 @@
+//install validator package from npm i validator --save
+
 var mongoose= require('mongoose');
-var user = mongoose.model('User',{
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash')
+
+var UserSchema =new mongoose.Schema({
     email:{
         type: String,
         required: true,
         trim: true,
-        minlength:1
-    }
+        minlength:1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid e-mail'
+        } 
+    },
+    password:{
+        type:String,
+        require:true,
+        minlength:6
+    },
+    tokens:[{
+        access:{
+            type:String,
+            required: true
+        },
+        token:{
+            type:String,
+            required: true
+        }
+    }]
 });
-module.exports={user};
+
+UserSchema.methods.toJSON = function(){
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject,['_id', 'email'])
+}
+
+UserSchema.methods.generateAuthToken = function(){
+    var user = this;
+    var access ='auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access},'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]);
+    user.save().then(()=>{
+        return token
+    });
+};
+
+var User = mongoose.model('User',UserSchema);
+module.exports={User};
--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -6,13 +6,27 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const port = process.env.PORT || 3000;
+const {ObjectID}=require('mongodb');
+
 var {Mongoose} = require('./db/mongoose');
 var {Todo} = require('./db/models/todo');
 var {User} = require('./db/models/user');
 
+const _ = require('lodash')
+
 var app=express();
 
 app.use(bodyParser.json());
+
+// app.use((req, res, next)=>{
+//     var now = new Date().toString();
+//    var log =now+ " : "+ req.method + req.url;
+//     //the page will freeze without next line
+//     fs.appendFile('server.log', log+'\n', (err)=>{if (err){console.log('unable to append file');}})
+//      next();
+
+// });
 
 app.post('/todos', (req, res)=>{
     //this line is to check request made body  => console.log(req.body);
@@ -37,7 +51,120 @@ app.get('/todos',(req, res)=>{
     });
 });
 
-app.listen(3000, ()=>{
+app.get('/todos/:id',(req, res)=>{
+     var id =req.params.id;
+
+    if(ObjectID.isValid(id)){
+        Todo.findById(id).then((todo)=>{
+            if (!todo){
+                //return 404 result if cannot find id
+                return res.status(404).send('cannot find id ');
+            }
+            res.send({todo});
+         },(e)=>{
+             res.status(404).send(e);
+         });
+       
+    }else{
+        return res.status(404).send("invalid id ");
+    }
+    
+});
+
+app.delete('/todos/:id', (req,res)=>{
+    var id =req.params.id;
+
+    if(ObjectID.isValid(id)){
+        Todo.findByIdAndDelete(id).then((todo)=>{
+            if (!todo){
+                //return 404 result if cannot find id
+                return res.status(404).send('cannot find id ');
+            }
+            console.log('removed this todos: ' + todo);
+            res.send({todo});
+         },(e)=>{
+             res.status(404).send(e);
+         });
+       
+    }else{
+        return res.status(404).send("invalid id ");
+    }
+
+});
+
+app.patch('/todos/:id', (req,res)=>{
+    /**todo describe this function in detail so future me does not become dumb ass
+     * 1. install lodash to use this route( sudo npm i lodash --save )
+     * 2. we use lodash pick so that the user control what user can/cannot change
+     *
+    */
+    
+    var id =req.params.id;
+
+    var body = _.pick(req.body, ['text', 'completed']);
+
+
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed)&& body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed=false;
+        body.completedAt = null;
+    }
+
+    console.log(id);
+    console.log(body);
+
+    Todo.findOneAndUpdate(id,{$set: body}, {new: true}).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+
+    }).catch((e)=>{
+        console.log(e);
+        res.status(400).send();
+    })
+
+});
+
+/**
+ * USERS
+ */
+app.post('/users', (req, res)=>{
+    //this line is to check request made body  => console.log(req.body);
+
+    var body = _.pick(req.body, ['email', 'password']);
+    
+
+    var user = new User(body);
+    
+
+    user.save().then(()=>{
+        return user.generateAuthToken();
+        //res.send(user);
+
+    }).then((token)=>{
+        //you can disable this "then" to stop user from automatically logged in after sigining up 
+        res.header('x-auth',token).send(user);
+
+    }).catch((e)=>{
+        res.status(400).send(e);
+    });
+});
+
+app.get('/users',(req, res)=>{
+   User.find().then((users)=>{
+        res.send({users});
+    },(e)=>{
+        res.status(400).send(e);
+    });
+});
+
+app.listen(port, ()=>{
     console.log('server is started');
 });
 
